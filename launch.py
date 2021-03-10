b57b21380bf09a0c27eb7d1e4f52efdda339e3c6
CharlieAprog <charlie.albietz.uni@gmail.com>
2021-03-10 17:30:55 +0100

    added labels

--- launch.py
+++ launch.py
@@ -1,8 +1,4 @@
-import glob
-from read_data import read_data
+from read_data import getData
 
-images_monet = glob.glob("Data/monet_jpg/*.jpg")
-images_photo = glob.glob("Data/photo_jpg/*.jpg")
-augs = glob.glob("Data/augs/*.jpg")
-#paintings, photos = read_data(images_monet, images_photo)
-paintings, photos = read_data(augs, images_photo)
+x, y = getData()
+print(x.shape, y.shape)
--- read_data.py
+++ read_data.py
@@ -4,16 +4,17 @@ from imgaug import augmenters as iaa
 from numpy import asarray
 from PIL import Image
 from tqdm import tqdm
+import glob
 
 
 def read_data(images_monet, images_photo):
     img_monet = []
-    for image in images_monet:
+    for image in tqdm(images_monet, desc='reading paintings'):
         with open(image, 'rb') as file:
             img = asarray(Image.open(file))
             img_monet.append(img)
     img_photo = []
-    for image in images_photo:
+    for image in tqdm(images_photo, desc = 'reading photos'):
         with open(image, 'rb') as file:
             img = asarray(Image.open(file))
             img_photo.append(img)
@@ -64,3 +65,21 @@ def augment(images):
     contrasted = contrast(cropped)
     for i, newImage in enumerate(contrasted):
         imageio.imwrite("Data/augs/%d.jpg" % (i,), newImage.astype(np.uint8))
+
+def combinePhotos(paintings, photos):
+    #5400 monet paintings, 7038 photos
+    ypaint = [1] * 5400
+    yphoto = [0] * 7038
+    y = np.array(ypaint + yphoto)
+
+    #combine both image sets
+    x = np.concatenate((paintings, photos), axis = 0)
+    return x, y 
+
+def getData():
+    images_photo = glob.glob("Data/photo_jpg/*.jpg")
+    augs = glob.glob("Data/augs/*.jpg")
+    paintings, photos = read_data(augs, images_photo)
+    print(f'paintings: {paintings.shape}\nphotos: {photos.shape}')
+    return combinePhotos(paintings, photos)
+
