822e1e9d6a1aa15524affd441288f03622333c32
CharlieAprog <charlie.albietz.uni@gmail.com>
2021-03-27 14:01:28 +0100

    changejfwhn

--- AutoEncoderMonet/AE.py
+++ AutoEncoderMonet/AE.py
@@ -65,7 +65,7 @@ class autoencoder(nn.Module):
             
             for i, (photo_real, monet_real) in enumerate(t):
                 photo_img, monet_img = photo_real.to(self.device), monet_real.to(self.device)
-                update_req_grad(self, False)
+                update_req_grad([self], False)
                 self.opt.zero_grad()
                 fake_monet = forward(photo_img)
                 loss = self.mse_loss(fake_monet, monet_img)
