5159b13d2d9531366332d6489ebb6bcaab8e6a3c
ritpanagiotis <ritpanagiotis@gmail.com>
2021-03-28 20:30:52 +0200

    woah

--- PanosCycleGan/cycleGan.py
+++ PanosCycleGan/cycleGan.py
@@ -111,23 +111,6 @@ def Convlayer(in_ch, out_ch, kernel_size=3, stride=2, use_leaky=True, use_inst_n
     )
 
 
-#class Grad_penalty(model, real, fake, device='cpu'):
-    #BATCH_SIZE, C, H, W = real.shape
-    #epsilon = torch.rand((BATCH_SIZE, 1, 1, 1)).repeat(1, C, H, W).to(device)
-    #inter_images = real * epsilon + fake * (1 - epsilon)
-    #mixed_scores = discriminator(inter_images)
-    #gradient = torch.autograd.grad(inputs =  inter_images,
-      #                             outputs = mixed_scores,
-     #                              grad_outputs=torch.ones_like(mixed_scores),
-     #                              create_graph = True,
-     #                              retain_graph = True,
-     #                              )[0]
-    #gradient = gradient.view(gradient.shape[0],-1)
-    #gradient_norm = gradient.norm(2,dim =1 )
-    #gradient_penalty = torch.mean((gradient_norm -1)**2)
-   # return gradient_penalty
-
-
 class CycleGAN(object):
     def __init__(self, in_ch, out_ch, epochs, device, start_lr=2e-4, lmbda=10, idt_coef=0.5, decay_epoch=0):
         self.epochs = epochs
